9e5225c2b975ca1c39a2da93e13b8b1245fd8e9f
Sankrant <sankrant@aquilaclouds.com>
2022-09-01 13:25:00 +0530

    introducing code-smells

---
+++ codesmells.go
@@ -0,0 +1,8 @@
+func code_smell1() {
+	prepare("This should be a constant") // Noncompliant; 'This should ...' is duplicated 3 times
+	execute("This should be a constant")
+	release("This should be a constant")
+}
+
+func code_smell2() { // Noncompliant
+}
